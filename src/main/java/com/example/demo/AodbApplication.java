dc7eabce74a6b185c62b363f6b77035e0b1e3b80
connorMurray <connor.murray@sita.aero>
2017-07-15 11:59:40 +0100

    Adding spring boot skeleton project

---
+++ src/main/java/com/example/demo/AodbApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AodbApplication.class, args);
+	}
+}
